13df2076a28bcbba464718b7c30547425b6a157d
jo1jun <andrew6072@naver.com>
2021-02-23 16:20:27 +0900

    day5

--- Autoencoder/basic_autoencoder.py
+++ Autoencoder/basic_autoencoder.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # # 오토인코더로 이미지의 특징을 추출하기
 
 import torch
@@ -13,6 +10,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 import numpy as np
+
+# Axes3D : matplotlib 에서 3차원의 plot 을 그리는 용도.
+# cm     : 데이터 포인트에 색상을 입히는 데 사용.
 
 
 # 하이퍼파라미터
@@ -34,7 +34,7 @@ train_loader = torch.utils.data.DataLoader(
     dataset     = trainset,
     batch_size  = BATCH_SIZE,
     shuffle     = True,
-    num_workers = 2
+    # num_workers = 2 # GPU 가 없는 내 PC 에서는 이 line 을 없애야 잘 작동한다. Colab 에서는 GPU 하므로 잘 작동.
 )
 
 
@@ -43,14 +43,14 @@ class Autoencoder(nn.Module):
         super(Autoencoder, self).__init__()
 
         self.encoder = nn.Sequential(
-            nn.Linear(28*28, 128),
+            nn.Linear(28*28, 128),      # input feature 는 28*28 개
             nn.ReLU(),
             nn.Linear(128, 64),
             nn.ReLU(),
             nn.Linear(64, 12),
             nn.ReLU(),
-            nn.Linear(12, 3),   # 입력의 특징을 3차원으로 압축합니다
-        )
+            nn.Linear(12, 3),           # 입력의 특징을 3차원으로 압축합니다 (시각화 하기 위해 3차원으로 압축.)
+        )                               # encoder 의 ouput 이 latent variable 이다.
         self.decoder = nn.Sequential(
             nn.Linear(3, 12),
             nn.ReLU(),
@@ -58,24 +58,25 @@ class Autoencoder(nn.Module):
             nn.ReLU(),
             nn.Linear(64, 128),
             nn.ReLU(),
-            nn.Linear(128, 28*28),
-            nn.Sigmoid(),       # 픽셀당 0과 1 사이로 값을 출력합니다
+            nn.Linear(128, 28*28),      # encoder 의 input feature 수와 동일한 feature 수로 복원한다.
+            nn.Sigmoid(),               # encoder 를 뒤집은 것과 같고 마지막에 Sigmoid 로 픽셀당 0~1 사이 값 출력.
         )
 
     def forward(self, x):
-        encoded = self.encoder(x)
-        decoded = self.decoder(encoded)
+        encoded = self.encoder(x)       # latent variable
+        decoded = self.decoder(encoded) # 복원된 이미지.
         return encoded, decoded
 
 
 autoencoder = Autoencoder().to(DEVICE)
-optimizer = torch.optim.Adam(autoencoder.parameters(), lr=0.005)
-criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(autoencoder.parameters(), lr=0.005)    # DL1 에서 공부한 Adam. README 참고.
+criterion = nn.MSELoss()                                            # Mean Squared Error Loss
 
 
 # 원본 이미지를 시각화 하기 (첫번째 열)
 view_data = trainset.data[:5].view(-1, 28*28)
-view_data = view_data.type(torch.FloatTensor)/255.
+view_data = view_data.type(torch.FloatTensor)/255.                  # (5 * 784) 크기의 0 ~ 1 사이값 tensor
+
 
 
 def train(autoencoder, train_loader):
@@ -85,32 +86,32 @@ def train(autoencoder, train_loader):
         y = x.view(-1, 28*28).to(DEVICE)
         label = label.to(DEVICE)
 
-        encoded, decoded = autoencoder(x)
+        encoded, decoded = autoencoder(x)               # forward
 
-        loss = criterion(decoded, y)
+        loss = criterion(decoded, y)                    # cacluate loss (복원과 원본 사이의 오차제곱합)
         optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        loss.backward()                                 # backward
+        optimizer.step()                                # update
 
 
 for epoch in range(1, EPOCH+1):
     train(autoencoder, train_loader)
 
     # 디코더에서 나온 이미지를 시각화 하기 (두번째 열)
-    test_x = view_data.to(DEVICE)
-    _, decoded_data = autoencoder(test_x)
+    test_x = view_data.to(DEVICE)                       # sample image 를 한 번 train 한 autoencoder에 넣기 (forward 만 수행)
+    _, decoded_data = autoencoder(test_x)               # sample 복원 image 를 get
 
-    # 원본과 디코딩 결과 비교해보기
-    f, a = plt.subplots(2, 5, figsize=(5, 2))
+    # 원본과 디코딩 결과 비교해보기 (시각화)
+    f, a = plt.subplots(2, 5, figsize=(5, 2))                               # 2행 5열 액자 구성
     print("[Epoch {}]".format(epoch))
     for i in range(5):
-        img = np.reshape(view_data.data.numpy()[i],(28, 28))
-        a[0][i].imshow(img, cmap='gray')
-        a[0][i].set_xticks(()); a[0][i].set_yticks(())
+        img = np.reshape(view_data.data.numpy()[i],(28, 28))                # 원본 이미지
+        a[0][i].imshow(img, cmap='gray')                                    # 1행에 원본 이미지 넣기
+        a[0][i].set_xticks(()); a[0][i].set_yticks(())                      # set_xticks, set_yticks 에 빈 튜플을 넣어 눈금자 표시를 없앤다.
 
-    for i in range(5):
-        img = np.reshape(decoded_data.to("cpu").data.numpy()[i], (28, 28))
-        a[1][i].imshow(img, cmap='gray')
+    for i in range(5):  
+        img = np.reshape(decoded_data.to("cpu").data.numpy()[i], (28, 28))  # 복원 이미지
+        a[1][i].imshow(img, cmap='gray')                                    # 2행에 원본 이미지 넣기
         a[1][i].set_xticks(()); a[1][i].set_yticks(())
     plt.show()
 
